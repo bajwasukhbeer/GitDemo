8c0a6c2ff5b7c7d80b9a924b220aa332810bcc29
bajwasukhbeer <bajwa.sukhbeersingh@gmail.com>
2020-08-29 22:27:21 +0530

    my first commit

---
+++ src/test/java/section20maven/Mavenjava/AppiumTest.java
@@ -0,0 +1,19 @@
+package section20maven.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void nativeAppAndroid()
+	{
+		System.out.println("nativeAppAndroid");
+	}
+	
+	@Test
+	public void IOSApp()
+	{
+		System.out.println("IOSApp");
+	}
+	
+}
---
+++ src/test/java/section20maven/Mavenjava/RESTAPITest.java
@@ -0,0 +1,20 @@
+package section20maven.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deletTwitter()
+	{
+		System.out.println("deletTwitter");
+	}
+	
+	
+}
